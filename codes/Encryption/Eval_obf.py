d006cb0d8a11c2c026ba370d41cbd985be7ba06f
Algorithms of KLEIN <92029487+cryptonite-core@users.noreply.github.com>
2025-10-08 21:51:26 +0600

    Add files via upload

---
+++ codes/Encryption/Eval_obf.py
@@ -0,0 +1,109 @@
+""" Decompiled by Exotic Hridoy """
+
+import os
+import time,sys
+import base64
+import marshal
+import zlib
+import platform
+import datetime
+import random
+from random import randint
+from rich.panel import Panel
+from rich import print as tulis
+from rich import print
+
+Hitam="[30m"
+Merah="[31m"
+Hijau="[32m"
+Kuning="[33m"
+Orange="[38;5;208m"
+Biru="[34m"
+Biru_muda="[36m"
+Pink="[38;5;205m"
+Violet="[35m"
+Abu="[90m"
+Orange_muda="[38;5;214m"
+Putih="[37m"
+
+Z2="[#000000]"  # Hitam
+M2="[#FF0000]"  # Merah
+H2="[#00FF00]"  # Hijau
+K2="[#FFFF00]"  # Kuning
+B2="[#00C8FF]"  # Biru
+U2="[#AF00FF]"  # Ungu
+N2="[#FF00FF]"  # Pink
+O2="[#00FFFF]"  # Biru Muda
+P2="[#FFFFFF]"  # Putih
+J2="[#FF8F00]"  # Jingga
+A2="[#AAAAAA]"  # Abu-Abu
+T2="[#FFA500]"  # Oranye
+
+def clear():
+    if "linux" in sys.platform.lower():
+        os.system("clear")
+    elif "win" in sys.platform.lower():
+        os.system("cls")# clear()
+
+class LambdaObfuscators:
+    def __init__(self):
+        current_time=datetime.datetime.now()
+        current_time_str=current_time.strftime("%A, %B %d, %Y %H:%M:%S")
+        self.mzb=f'''# Time : {current_time_str}
+# Platform : {platform.system()}-{platform.machine()}
+# Obfuscated by : Ferly Afriliyan (Ryougaa Hideki)
+
+Ryougaa_ = (\n'''
+        denventa_lines=[f'"000000000000000","000000000000000","000000000000000","000000000000000","000000000000000", {str(["import", "as", "from", "marshal", "zlib", "base64"])}, "Ryougaa_Hidekii__=(eval((lambda", "exec(", ";exec(Ryougaa_Hidekii__)"'for _ in range(140,1409)]
+        self.mzb+=",\n".join(denventa_lines)
+        self.mzb+=f'''\n)
+
+# Time : {current_time_str}
+# Platform : {platform.system()}-{platform.machine()}
+# Obfuscated by : Ferly Afriliyan (Ryougaa Hideki)
+# Variable teks : {len(denventa_lines)} Line
+
+
+recode_kontol = %s;Ryougaa_Hidekii__=(eval((lambda ____,__,_ : ____.join([_(___) for ___ in __]))('',[95, 95, 105, 109, 112, 111, 114, 116, 95, 95, 40, 39, 109, 97, 114, 115, 104, 97, 108, 39, 41, 46, 108, 111, 97, 100, 115],chr))(recode_kontol));exec(Ryougaa_Hidekii__)
+
+Hidekii_ = (\n'''
+        denventa_lines=[f'"000000000000000","000000000000000","000000000000000","000000000000000","000000000000000", {str(["import", "as", "from", "marshal", "zlib", "base64"])}, "Ryougaa_Hidekii__=(eval((lambda", "exec(", ";exec(Ryougaa_Hidekii__)"'for _ in range(140,1409)]
+        self.mzb+=",\n".join(denventa_lines)
+        self.mzb+=f'''\n)'''
+
+    def encrypt_file(self,input_file,output_file,count):
+        try:
+            __obf=open(input_file,"rb").read()
+            __com=compile(__obf,"","exec")
+            __repr__=repr(marshal.dumps(__com))
+            with open(output_file,"w")as f:
+                f.write(self.mzb % __repr__)
+            total_iterations=count
+            current_iteration=0
+            while current_iteration<total_iterations:
+                obf__=open(output_file,"rb").read()
+                ___compile__=compile(obf__,"","exec")
+                ___repr_compile___=repr(marshal.dumps(___compile__))
+                with open(output_file,"w")as f:
+                    f.write(self.mzb % ___repr_compile___)
+                current_iteration+=1
+                loading_percentage=(current_iteration/total_iterations)*100
+                print(f"Encrypting: [{loading_percentage:.2f}%]",end="\r")
+                time.sleep(0.0)
+            print(f"\n{H2}[{A2}!!{H2}]{P2} Berhasil, File tersimpan di {A2}: {output_file}")
+            exit()
+        except Exception as e:
+            print(f"{M2}[{A2}•{M2}]{P2} Error : {P2}\n└──>{M2}[{A2}•{M2}] {P2}Isi Yang Benar {M2}!")
+
+if __name__=="__main__":
+    encryptor=LambdaObfuscators()
+    clear()
+    tulis(Panel("[#00BFFF]    ╔═╗┬  ┬┌─┐┬   ┌─┐┌┐ ┌─┐[#808080]Python3 with lambda[#00BFFF]\n    ║╣ └┐┌┘├─┤│───│ │├┴┐├┤\n    ╚═╝ └┘ ┴ ┴┴─┘ └─┘└─┘└ \n        ",title="[#FF8F00][ [#00FF33]By Ferly Afriliyan [#FF8F00]]",
+        subtitle="[#FF8F00][ [#00FF33]Obfuscate Your Python File [#FF8F00]]",style="#FFFFFF"))
+    input_file=input(f"└──>{Hijau}[{Abu}•{Hijau}]{Putih} Input File {Abu}: {Putih}")
+    count=int(input(f"     └──>{Biru}[{Abu}+{Biru}]{Putih} Count {Orange_muda}({Putih}Ex{Orange_muda}) •4• : {Putih}"))
+    if count<11:
+        output_file=input(f"     └──>{Hijau}[{Abu}•{Hijau}]{Putih} Output File {Abu}: {Putih}")
+        encryptor.encrypt_file(input_file,output_file,count)
+    else:
+        print(f"{B2}[{A2}!{B2}]{P2} Count must be < 10")
---
+++ codes/Encryption/Felry_obf.py
@@ -0,0 +1,141 @@
+""" Decompiled by Exotic Hridoy """
+
+import sys
+import os
+import random
+from pystyle import*
+import marshal as marshal
+import base64
+import time
+
+def clear():
+    if "linux" in sys.platform.lower():
+        os.system("clear")
+    elif "win" in sys.platform.lower():
+        os.system("cls")
+k="[1;33m"  # Warna Kuning
+a="[1;30m"  # Warna Hitam/Abu-Abu
+m="[1;31m"  # Warna Merah
+h="[1;32m"  # Warna Hijau
+p="[1;37m"  # Warna Putih
+b="[1;34m"  # Warna Biru
+v="[1;35m"  # Warna Violet
+u="[1;36m"  # Warna Ungu
+j="[1;38;5;202m"  # Warna Jingga
+
+class Hyperion():
+    serializer=marshal
+    def Alt(text:str,evalCode:bool=True)->str:
+        formated="+".join(f'chr({char})'for char in[ord(char_)for char_ in text])
+        return f'eval(eval({formated!r}))'if evalCode else f'eval({formated!r})'
+    hhidedText="GG! You deobfuscated the code! obfuscated by Hyperion"
+    exceptionCode="input(\"Don't try to skid Hyperion obfuscation !\");exit(0)"
+    GithubLink="https://github.com/ferlyafriliyan/Hyperion"
+    infos={
+        "__author__":"Ferly Afriliyan",
+        "__madeBy__":"Hyperion",
+        "__git__":GithubLink,
+}
+    gatewayKey=random.randint(0,10000)
+
+    def Gateway()->str:
+        obj=globals()["__selfObject__"]
+        interpreterObj=globals()["__InterpreterObject__"]
+        key=globals()["__key__"]
+        code=interpreterObj.code["bytes"]
+        obj.executed=True
+        return((key*8/1.5),code)
+    comment="Obfuscated with Hyperion"
+    encCommend=" + ".join(f'chr({char})'for char in[ord(char_)for char_ in comment])
+    checkInfos=" and ".join(f'globals().get("{key}") == {value!r}'for key,value in infos.items())+\
+        f' and ("# " and {encCommend}) in open(__import__("os").path.abspath(__file__), "r", encoding = "utf-8").read() '
+    interpreterClass="\nclass Interpreter():\n    def __init__(self, code: str, layersFunction: bytes, module, globals_, backend: bytes = b'') -> None:\n        self.__module = module\n        self.layersFunction = layersFunction\n        self.__globals = globals_\n        self.code = {'bytes': code, 'str': str(code)}\n        self.__backend = backend\n\n    def __tunnel(self) -> Gateway: return Gateway(self.__backend, 7605, __module = self.__module, __globals = self.__globals, interpreter = self)\n\n    def Run(self) -> None:\n        decoder = self.__getobject__()\n        gate = self.__tunnel().Pass()\n        exec(eval(MARSHALMODULE.loads(decoder),\n                  {'__selfObject__': self, '__module': self.__module, '__sr_m': eval(eval('chr(95)+chr(95)+chr(105)+chr(109)+chr(112)+chr(111)+chr(114)+chr(116)+chr(95)+chr(95)+chr(40)+chr(34)+chr(109)+chr(97)+chr(114)+chr(115)+chr(104)+chr(97)+chr(108)+chr(34)+chr(41)')), '__globals': self.__globals, 'gate': gate}),\n                  self.__globals)\n\n    def __getobject__(self) -> object:\n        func = self.layersFunction\n        return self.__module.b64decode(func)\n        "[1:-1].replace("MARSHALMODULE",Alt("__import__(\"marshal\")")).replace("GATEWAYKEY",str(gatewayKey))
+    gatewayClass="\nclass Gateway():\n    def __init__(self, way: bytes, key: int, **ext) -> None:\n        self.way = way\n        self.key = key\n        self.module__ = ext.get('__module', None)\n        self.__globals = ext.get('__globals', None)\n        self.__module = ext.get('__module', None)\n        self.__interpreter = ext.get('interpreter', None)\n\n    def Pass(self):\n        exec(MARSHALMODULE.loads(self.module__.b16decode(self.way)),\n             {'__selfObject__': self, '__key__': self.key, '__module': self.module__, '__globals': self.__globals, '__InterpreterObject__': self.__interpreter})\n        return self\n    "[1:-1].replace("MARSHALMODULE",Alt("__import__(\"marshal\")"))
+
+    def RemoveLayers()->str:
+        _globals=globals()["__globals"]
+        if not globals().get("gate"):
+            return
+        obj=globals()["__selfObject__"]
+        module=globals()["__module"]
+        code=obj.code["bytes"]
+        code=module.b85decode(code)
+        code=module.b64decode(code)
+        code=module.b32decode(code)
+        code=module.b16decode(code)
+        code=globals()["__sr_m"].loads(code)
+        return code
+
+    def Obfuscate(code:str)->str:
+        sys.setrecursionlimit(1000000)
+        _code=code
+        program="{'__name__': '__main__'}"
+        runCode=f"""
+if not ({Hyperion.checkInfos}): {Hyperion.exceptionCode}
+exec({_code!r}, {program})
+"""[1:-1]
+        code__=Hyperion.serializer.dumps(compile(runCode,"Hyperion","exec"))
+        infos_="\n".join(f'{key} = {value!r}'for key,value in Hyperion.infos.items())
+        code__=base64.b16encode(code__)
+        code__=base64.b32encode(code__)
+        code__=base64.b64encode(code__)
+        code__=base64.b85encode(code__)
+        code_=f"""
+{infos_}
+
+# {Hyperion.comment}
+import marshal, base64
+{Hyperion.gatewayClass}
+{Hyperion.interpreterClass}
+
+if __name__ == '__main__':
+    try:
+        Interpreter({code__!r},
+            {base64.b64encode(Hyperion.serializer.dumps(Hyperion.RemoveLayers.__code__))!r},
+            {Hyperion.Alt('__import__("base64")')}, globals(),
+            {base64.b16encode(Hyperion.serializer.dumps(Hyperion.Gateway.__code__))!r}
+        ).Run()
+    except (KeyboardInterrupt, Exception) as e:
+        exit(f"[Error] {{str(e).capitalize()}}!")
+# Lu Kontool
+"""[1:-1]
+        return code_
+
+colors=Col.red_to_purple
+input_=0.01
+interval=0.1
+banner="\n██╗  ██╗██╗   ██╗██████╗ ███████╗██████╗ ██╗ ██████╗ ███╗   ██╗\n██║  ██║╚██╗ ██╔╝██╔══██╗██╔════╝██╔══██╗██║██╔═══██╗████╗  ██║\n███████║ ╚████╔╝ ██████╔╝█████╗  ██████╔╝██║██║   ██║██╔██╗ ██║\n██╔══██║  ╚██╔╝  ██╔═══╝ ██╔══╝  ██╔══██╗██║██║   ██║██║╚██╗██║\n██║  ██║   ██║   ██║     ███████╗██║  ██║██║╚██████╔╝██║ ╚████║\n╚═╝  ╚═╝   ╚═╝   ╚═╝     ╚══════╝╚═╝  ╚═╝╚═╝ ╚═════╝ ╚═╝  ╚═══╝\n"[:-1]
+
+def Main():
+    whiteChars=list("═╝╚╔╗║")
+    print(Colorate.Format(banner,whiteChars,Colorate.Vertical,colors,Col.white))
+    path=input(f"{m}Drag and drop the file to obfuscate => {p}")
+    path=os.path.normpath(path)
+    if not os.path.exists(path):
+        Colorate.Error("File not found.")
+        time.sleep(input_)
+        exit()
+    if not os.path.isfile(path):
+        print("Not a file. !")
+        time.sleep(interval)
+        exit()
+    output_filename=input(f"{m}Enter the output file name ({u}including {p}'{b}.py{p}' {u}extension): {p}")
+    time.sleep(input_)
+    code=""
+    print(f"{k}[{a}!{k}] {p}WARN: The __name__ variable will be set to '__main__'. Press Enter to continue...")
+    time.sleep(0.1)
+    with open(path,"r",encoding="utf-8")as file:
+        code=file.read()
+    code=Hyperion.Obfuscate(code)
+    with open(output_filename,"w",encoding="utf-8")as file:
+        file.write(code)
+    print(f"{h}[{a}!{h}] {p}Successfully obfuscated and saved to {j}{output_filename}{p}!")
+    time.sleep(0.01)
+    exit()
+if __name__=="__main__":
+    System.Title("Hyperion - By Ferly Afriliyan_")
+    System.Size(150,47)
+    try:
+        Main()
+    except(KeyboardInterrupt,Exception)as e:
+        exit(f"[Error] {str(e).capitalize()}!")
